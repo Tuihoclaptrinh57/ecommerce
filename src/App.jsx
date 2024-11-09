3a57aa4a12c8a9583c94bce2906498a59b099058
tuihoclaptrinh <hoangtuananh57tht1@gmail.com>
2024-11-09 21:06:49 +0700

    [DEV-5] - Configuring new alias in vite.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import "./App.css"
+import Hello from "@components/hello/Hello"
 
 function App() {
-  const zz = (x) => {
-    console.log("aaaa")
-  }
-
-  const arr = [1, 2, 3]
-
   return (
     <div>
-      {" "}
-      Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello
-      Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello
-      Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello
-      Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello
-      Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello
-      Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello
-      Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce Hello Ecommerce{" "}
+      <Hello /> Ecommerce
     </div>
   )
 }
